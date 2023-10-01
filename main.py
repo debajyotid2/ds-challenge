03cd60033ab6aa831959320f8c1f927dc774b310
Debajyoti Debnath <ddebnath@purdue.edu>
2023-10-01 11:38:20 -0400

    Add model training and evaluation.

--- main.py
+++ main.py
@@ -1,9 +1,63 @@
 """
 Main script for the data science challenge
 """
+import warnings
+
+warnings.filterwarnings("ignore")
 import argparse
 from pathlib import Path
-from src.data import load_data, preprocess_data, extract_features
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from sklearn.metrics import (
+    balanced_accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    classification_report,
+    ConfusionMatrixDisplay,
+)
+from sklearn.preprocessing import RobustScaler
+from sklearn.pipeline import Pipeline
+
+from src.data import (
+    load_data,
+    preprocess_data,
+    extract_features,
+    generate_labels,
+    generate_train_val_test_idxs,
+)
+from src.model import load_model
+
+
+def train_and_evaluate(
+    model: str,
+    x_train: pd.DataFrame,
+    y_train: pd.Series,
+    x_test: pd.DataFrame,
+    y_test: pd.Series,
+):
+    """Train and evaluate a model."""
+    classifier = load_model(model)
+    pipeline_list = []
+    if model not in ["decision tree", "random forest", "xgb"]:
+        pipeline_list.append(("scaler", RobustScaler()))
+    pipeline_list.append(("classifier", classifier()))
+
+    pipeline = Pipeline(pipeline_list)
+    pipeline.fit(x_train, y_train)
+    y_pred = pipeline.predict(x_test)
+    print(f"Model {model}:")
+    print(
+        f" Balanced accuracy score: {balanced_accuracy_score(y_test,y_pred)*100:.2f}%."
+    )
+    print(f" Precision score: {precision_score(y_test,y_pred)*100:.2f}%.")
+    print(f" Recall score: {recall_score(y_test,y_pred)*100:.2f}%.")
+    print(f" F1 score: {f1_score(y_test,y_pred)*100:.2f}%.")
+    ConfusionMatrixDisplay.from_estimator(pipeline, x_test, y_test)
+    plt.show()
 
 
 def main():
@@ -12,11 +66,34 @@ def main():
     )
     parser.add_argument("-p", "--data_path", help="Path to csv/text/excel data file.")
     parser.add_argument(
-        "-w",
-        "--window_size",
+        "--feature_window_size",
         type=int,
         help="Window size to consider for rolling statistics for features. Default: 10",
         default=10,
+    )
+    parser.add_argument(
+        "--failure_window_size",
+        type=int,
+        help="Window size or number of cycles within which failure must be predicted. Default: 20",
+        default=20,
+    )
+    parser.add_argument(
+        "--val_frac",
+        type=float,
+        help="Fraction of machines in the validation set. Default: 0.10",
+        default=0.10,
+    )
+    parser.add_argument(
+        "--test_frac",
+        type=float,
+        help="Fraction of machines in the test set. Default: 0.10",
+        default=0.10,
+    )
+    parser.add_argument(
+        "--seed",
+        type=int,
+        help="Random number seed. Default: 42",
+        default=42,
     )
     args = parser.parse_args()
 
@@ -27,11 +104,35 @@ def main():
     if not data_path.exists():
         raise ValueError("Must supply a valid data path.")
 
+    # Set random number generator seed
+    np.random.seed(args.seed)
+
+    # Load, preprocess and feature engineer data
     raw = load_data(args.data_path)
     preprocessed = preprocess_data(raw)
-    breakpoint()
-    features = extract_features(preprocessed, args.window_size)
-    breakpoint()
+    features = extract_features(preprocessed, args.feature_window_size)
+    features = features.sort_values(by=[0, 1], ascending=True).reset_index(drop=True)
+
+    # Get labels
+    labels = generate_labels(features[0], args.failure_window_size)
+
+    # Split data into training, validation and test
+    train_idxs, val_idxs, test_idxs = generate_train_val_test_idxs(
+        features[0], args.val_frac, args.test_frac, args.seed
+    )
+
+    features.drop(columns=[0, 1], inplace=True)
+    features.columns = [str(col) for col in features.columns]
+
+    x_train, y_train = features.loc[train_idxs, :], labels.loc[train_idxs]
+    x_val, y_val = features.loc[val_idxs, :], labels.loc[val_idxs]
+    x_test, y_test = features.loc[test_idxs, :], labels.loc[test_idxs]
+
+    # Train model
+    models = ["logistic", "svc", "decision tree", "random forest", "xgb", "mlp"]
+
+    for model in models:
+        train_and_evaluate(model, x_train, y_train, x_test, y_test)
 
 
 if __name__ == "__main__":
--- src/data.py
+++ src/data.py
@@ -3,14 +3,19 @@ Functions to load and preprocess data.
 """
 from typing import Literal
 from pathlib import Path
+from functools import reduce
+import numpy as np
 import pandas as pd
 
 
 def get_non_cat_cols(data: pd.DataFrame) -> set[str]:
-    """Returns a set of columns from data that are non-categorical features."""
+    """
+    Returns a set of columns from data that are non-categorical features.
+    Columns with datatype object, string or int64 are considered categorical.
+    """
     cat_cols = set()
     for count, val in enumerate(data.dtypes):
-        if val not in ["object", "str"]:
+        if val not in ["object", "str", "int64"]:
             continue
         cat_cols.add(data.columns[count])
     return set(data.columns) - cat_cols
@@ -27,6 +32,19 @@ def preprocess_data(raw_data: pd.DataFrame) -> pd.DataFrame:
     processed = raw_data.copy(deep=True)
     # Drop columns with null values
     processed = processed.dropna(axis=1, how="any")
+
+    # Convert column representing time cycles to float
+    processed[1] = processed[1].astype(np.float64)
+
+    # Convert all sensor signal columns to float
+    processed.iloc[:, 3:] = processed.iloc[:, 3:].astype(np.float64)
+
+    # Remove non-categorical features that have zero variation, i.e. do
+    # not change over time
+    non_cat_cols = get_non_cat_cols(processed)
+    unchanging_cols = [col for col in non_cat_cols if processed[col].std() <= 0.0001]
+    processed.drop(columns=unchanging_cols, inplace=True)
+
     return processed
 
 
@@ -64,6 +82,24 @@ def get_rolling_statistics(
     return rolling_stat_feature
 
 
+def get_relative_change_signal(
+    feature: pd.Series, feature_moving_mean: pd.Series, window_size: int
+) -> pd.Series:
+    """
+    Creates a new feature containing the signal to mean ratio to emphasize
+    sudden spikes or valleys in the signal.
+    """
+    signal_to_mean = feature.copy(deep=True)
+    signal_to_mean[: window_size - 1] = 0.0
+    feature_moving_mean = feature_moving_mean.copy(deep=True).replace(0.0, 1.0)
+    signal_to_mean[window_size - 1 :] = (feature / feature_moving_mean)[
+        window_size - 1 :
+    ]
+    signal_to_mean.fillna(0.0, inplace=True)
+    signal_to_mean.name = f"{feature.name}_relative_change"
+    return signal_to_mean
+
+
 def extract_features(preprocessed_data: pd.DataFrame, window_size: int) -> pd.DataFrame:
     """
     Performs feature engineering on the cleaned dataset to prepare
@@ -71,6 +107,9 @@ def extract_features(preprocessed_data: pd.DataFrame, window_size: int) -> pd.Da
     """
     features = preprocessed_data.copy(deep=True)
     non_cat_cols = get_non_cat_cols(features)
+
+    # Remove time (number of cycles) as a column to use for feature engineering
+    non_cat_cols.remove(1)
 
     # Get rolling statistics
     moving_stats = []
@@ -82,4 +121,76 @@ def extract_features(preprocessed_data: pd.DataFrame, window_size: int) -> pd.Da
             )
     features = pd.concat([features, *moving_stats], axis=1)
 
+    # Get the signal to mean ratio to emphasize sudden spikes or valleys
+    # in signal.
+    rel_change_stats = []
+    for col in non_cat_cols:
+        mean_col = f"{col}_moving_mean"
+        rel_change_stats.append(
+            get_relative_change_signal(features[col], features[mean_col], window_size)
+        )
+    features = pd.concat([features, *rel_change_stats], axis=1)
+
     return features
+
+
+def generate_labels(
+    machine_id_column: pd.Series, failure_window_size: int = 20
+) -> pd.Series:
+    """
+    Based on the failure window size, generate target column indicating 1
+    when a failure will occur within the next failure_window_size days and 0
+    if not. It is assumed that the machine_id_column is sorted by machine ID
+    and time cycle.
+    """
+    machine_ids = machine_id_column.unique()
+    label_col = pd.Series(
+        index=machine_id_column.index,
+        data=np.zeros_like(machine_id_column.values, dtype=np.float32),
+        name="failure",
+    )
+    for machine_id in machine_id_column:
+        idxs = label_col[machine_id_column == machine_id][-failure_window_size:].index
+        label_col[idxs] = 1.0
+    return label_col
+
+
+def generate_train_val_test_idxs(
+    machine_id_column: pd.Series,
+    val_frac: float = 0.10,
+    test_frac: float = 0.10,
+    seed: int = 42,
+) -> tuple[pd.Index, pd.Index, pd.Index]:
+    """
+    Given a set of machine IDs, sets aside val_frac fraction of machines for
+    the validation set, test_frac fraction for the test set and the remaining
+    for the training set. Returns the respective indexes from the machine id
+    column.
+    """
+
+    def gather_subset_idxs(machine_ids: np.ndarray) -> pd.Index:
+        filt = [(machine_id_column == id_val) for id_val in machine_ids]
+        filt = reduce(lambda x, y: x | y, filt)
+        return machine_id_column[filt].index
+
+    machine_ids = machine_id_column.unique()
+    rng = np.random.default_rng(seed=seed)
+    rng.shuffle(machine_ids)
+    num_val, num_test = int(val_frac * len(machine_ids)), int(
+        test_frac * len(machine_ids)
+    )
+    val_machine_ids = machine_ids[:num_val]
+    test_machine_ids = machine_ids[num_val : num_val + num_test]
+    train_machine_ids = machine_ids[num_val + num_test :]
+
+    # Gather indices for each subset
+    train_idxs = gather_subset_idxs(train_machine_ids)
+    val_idxs = gather_subset_idxs(val_machine_ids)
+    test_idxs = gather_subset_idxs(test_machine_ids)
+
+    # Shuffle before returning
+    rng.shuffle(train_idxs.to_numpy())
+    rng.shuffle(val_idxs.to_numpy())
+    rng.shuffle(test_idxs.to_numpy())
+
+    return train_idxs, val_idxs, test_idxs
